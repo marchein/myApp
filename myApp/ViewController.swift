26cc1ee1cb14b976ed16f976217d0709a6e5a5fd
Marc Hein <info@marc-hein.de>
2019-02-22 18:01:41 +0100

    Initial Commit

---
+++ myApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myApp
+//
+//  Created by Marc Hein on 22.02.19.
+//  Copyright © 2019 Marc Hein Webdesign. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
